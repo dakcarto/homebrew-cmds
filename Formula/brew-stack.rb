0a0b124bed84438b17071dfa77234f8d429c3591
Larry Shaffer <lshaffer@boundlessgeo.com>
2016-12-10 16:10:23 -0700

    Cleanup audit of brew-stack

--- Formula/brew-stack.rb
+++ Formula/brew-stack.rb
@@ -1,6 +1,5 @@
-require "formula"
-
 class BrewStack < Formula
+  desc "External command to create fully built-as-bottle Homebrew prefix"
   homepage "https://github.com/dakcarto/homebrew-cmds/"
   url "https://github.com/dakcarto/homebrew-cmds.git"
   version "1.0.0"
@@ -11,5 +10,9 @@ class BrewStack < Formula
     bin.install "External-Cmds/brew-stack.rb"
     (bin/"brew-stack.rb").chmod 0755
     bash_completion.install "External-Cmds/brew_stack_completion.sh"
+  end
+
+  test do
+    #
   end
 end
